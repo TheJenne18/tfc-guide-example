593c1a390c4bed82766afd6768add853a36daeaa
TheJenne18 <jens.polspoel@gmail.com>
2021-12-17 15:32:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ resource "aws_instance" "ubuntu" {
 
   tags = {
     Name                 = var.instance_name
-    "Linux Distribution" = "Ubuntu - Jens"
+    "Linux Distribution" = "Ubuntu"
   }
 }
